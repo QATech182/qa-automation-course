ef1cfb0d499da0269d829793ccb2c22220f403dd
Eric <kyawkyawaung.connect@gmail.com>
2026-01-29 19:23:36 -0500

    error.js File Added and Update functions.js

---
+++ Week-3/errors.js
@@ -0,0 +1,16 @@
+// Syntax Error: Mistakes in your code structure
+// Reference Error: Using a variable that doesn't exist
+// Type Error: Using a value in a way thatʼs not allowed
+// Range Error: Number out of allowed range
+
+// SyntaxError
+
+// ReferenceError
+let x = 20;
+console.log(x);
+
+// TypeError
+
+// RangeError
+console.log("hi".repeat(3));
+//console.log("hi".repeat(-3));
---
+++ Week-3/scope.js
@@ -0,0 +1,1 @@
+console.log("hi".repeat(3));
