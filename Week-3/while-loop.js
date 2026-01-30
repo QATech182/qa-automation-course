ca84050f7ef8ff5461d7415678a5f4f08fed0c5a
Eric <kyawkyawaung.connect@gmail.com>
2026-01-29 21:51:04 -0500

    while-loop.js Added

---
+++ Week-3/while-loop.js
@@ -0,0 +1,18 @@
+// for (let i = 1; i <= 5; i++) {
+//   // i +=2 if you want to increase 2
+//   // to reassign the varaiable, we used the let NOT const
+//   console.log("Count:" + i);
+// }
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log("Count:" + i);
+//   i++; // without this, this will run the forever
+// }
+
+// let i = 1;
+// let isCorrectPasswordLengthCorrect = 8;
+// while (isCorrectPasswordLengthCorrect <= 5) {
+//   console.log("Count:" + isCorrectPasswordLengthCorrect);
+//   isCorrectPasswordLengthCorrect++; // without this, this will run the forever
+// }
