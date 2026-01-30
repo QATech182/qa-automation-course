3ff75f0e3c63e1f0748568722c5ee2e9962dd95f
Eric <kyawkyawaung.connect@gmail.com>
2026-01-29 22:20:24 -0500

    for-of-loop.js Added

---
+++ Week-3/for-of-loop.js
@@ -0,0 +1,18 @@
+// for (let i = 1; i <= 5; i++) {
+//   // i +=2 if you want to increase 2
+//   // to reassign the varaiable, we used the let NOT const
+//   console.log("Count:" + i);
+// }
+
+const numbers = [1, 2, 3, 4, 5];
+
+// color of colors
+for (const num of numbers) {
+  console.log(num);
+}
+
+const colo = [1, 2, 3, 4, 5];
+
+for (const num of numbers) {
+  console.log(num);
+}
